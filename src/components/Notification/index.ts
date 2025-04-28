c2503b7a8bf117c99d5987bbedeb82569a474646
Huespal <Huespal@users.noreply.github.com>
2025-04-28 11:19:15 +0200

    feat: add Notification component

--- src/components/Notification/index.ts
+++ src/components/Notification/index.ts
@@ -1,32 +1,58 @@
 import { unsubscribe } from '@/core/socket';
 import { subscribeNotifications } from '@/domain/notification/socket';
 import { Notification } from '@/domain/notification/types';
+import './style.css';
 
 export const renderNotifications = async (container: HTMLDivElement) => {
+  let notificationAmount = 0;
+
   const update = (notification: Notification) => {
-    const notificationDiv = document.getElementById('notification-name')!;
-    notificationDiv.innerHTML = `${notification.username}`;
+    if (notificationAmount === 0) render();
+
+    notificationAmount += 1;
+
+    const notificationNameDiv = document.getElementById('notification-name')!;
+    notificationNameDiv.innerHTML = `${notification.username}`;
+
+    if (notificationAmount > 9) notificationAmount = 9;
+    const notificationAmountDiv = document.getElementById('notification-amount')!;
+    notificationAmountDiv.innerHTML = `${notificationAmount}`;
   }
 
   let webSocket = subscribeNotifications(update);
 
-  container.innerHTML = `
-    <div id="notification-name"></div>
-    <button id="toggle-notifications">Turn off</button>
-  `;
+  const render = () => {
+    container.innerHTML = `
+      <div class="notification-content">
+        <figure>
+          <img
+            width="30"
+            height="30"
+            src="https://img.icons8.com/ios-glyphs/30/filled-appointment-reminders.png"
+            alt="Notification Bell"
+          />
+          <div id="notification-amount" class="notification-amount"></div>
+        </figure>
+        <p>New document added by</p>
+        <strong id="notification-name"></strong> 
+        <button id="notification-toggle">Turn off</button>
+      </div>`;
 
-  const button = document.getElementById('toggle-notifications')!;
+    const button = document.getElementById('notification-toggle');
 
-  const onToggle = () => {
-    const isOn = button.innerText === 'Turn off';
-    if (isOn) {
-      unsubscribe(webSocket);
-      button.innerText = `Turn on`;
-    } else {
-      webSocket = subscribeNotifications(update);
-      button.innerText = `Turn off`;
+    if (button) {
+      const onToggle = () => {
+        const isOn = button.innerText === 'Turn off';
+        if (isOn) {
+          unsubscribe(webSocket);
+          button.innerText = `Turn on`;
+        } else {
+          webSocket = subscribeNotifications(update);
+          button.innerText = `Turn off`;
+        }
+      }
+
+      button.addEventListener('click', onToggle);
     }
-  }
-
-  button.addEventListener('click', onToggle);
+  };
 }
