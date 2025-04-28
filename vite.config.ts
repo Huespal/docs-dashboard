82663407d84aed7f1f928bfb6636663c3fe3ebe9
Huespal <Huespal@users.noreply.github.com>
2025-04-28 11:18:15 +0200

    test: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   server: {
     port: 3000
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    onConsoleLog: () => false,
+    coverage: {
+      include: ['src/components/*']
+    }
   }
 })
